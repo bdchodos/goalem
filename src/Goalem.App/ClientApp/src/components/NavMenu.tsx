8e3385e959ec7718c21b78bd130ba3ec7b76a257
Pierce Lauring <pclauring@gmail.com>
2020-10-12 12:33:54 -0400

    Logout Button Added to NavMenu

--- src/Goalem.App/ClientApp/src/components/NavMenu.tsx
+++ src/Goalem.App/ClientApp/src/components/NavMenu.tsx
@@ -1,34 +1,69 @@
-import * as React from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import * as React from "react";
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import LogoutButton from "./LogoutButton";
+import "./NavMenu.css";
 
-export default class NavMenu extends React.PureComponent<{ isAuthenticated: boolean }, { isOpen: boolean }> {
+export default class NavMenu extends React.PureComponent<
+  { isAuthenticated: boolean },
+  { isOpen: boolean }
+> {
   public state = {
-    isOpen: false
+    isOpen: false,
   };
 
   public render() {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3" light>
+        <Navbar
+          className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3"
+          light
+        >
           <Container>
-            <NavbarBrand tag={Link} to="/">Goalem.App</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">
+              Goalem.App
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggle} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={this.state.isOpen} navbar>
+            <Collapse
+              className="d-sm-inline-flex flex-sm-row-reverse"
+              isOpen={this.state.isOpen}
+              navbar
+            >
               <ul className="navbar-nav flex-grow">
                 {this.props.isAuthenticated && (
                   <React.Fragment>
                     <NavItem>
-                      <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                      <NavLink tag={Link} className="text-dark" to="/">
+                        Home
+                      </NavLink>
                     </NavItem>
                     <NavItem>
-                      <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                      <NavLink tag={Link} className="text-dark" to="/counter">
+                        Counter
+                      </NavLink>
                     </NavItem>
                     <NavItem>
-                      <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                      <NavLink
+                        tag={Link}
+                        className="text-dark"
+                        to="/fetch-data"
+                      >
+                        Fetch data
+                      </NavLink>
                     </NavItem>
-                  </React.Fragment>)}
+                    <NavItem>
+                      <LogoutButton />
+                    </NavItem>
+                  </React.Fragment>
+                )}
               </ul>
             </Collapse>
           </Container>
@@ -39,7 +74,7 @@ export default class NavMenu extends React.PureComponent<{ isAuthenticated: bool
 
   private toggle = () => {
     this.setState({
-      isOpen: !this.state.isOpen
+      isOpen: !this.state.isOpen,
     });
-  }
+  };
 }
