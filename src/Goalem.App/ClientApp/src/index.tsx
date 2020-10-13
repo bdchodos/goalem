a31efa5a883b1c04e46e357aa16b651f0577e158
Pierce Lauring <pclauring@gmail.com>
2020-10-13 16:23:12 -0400

    Add Client ID and Doman to env.local file

--- src/Goalem.App/ClientApp/src/index.tsx
+++ src/Goalem.App/ClientApp/src/index.tsx
@@ -1,26 +1,31 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
-import configureStore from './store/configureStore';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Auth0Provider } from '@auth0/auth0-react';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import configureStore from "./store/configureStore";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // Create browser history to use in the Redux store
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
+const baseUrl = document
+  .getElementsByTagName("base")[0]
+  .getAttribute("href") as string;
 const history = createBrowserHistory({ basename: baseUrl });
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
 const store = configureStore(history);
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.render(
   <Auth0Provider
-    domain='dev-xcefc0h1.auth0.com'
-    clientId='vbQmY2v8TbdxDuO1bBeoymIm73Laa4Tt'
+    domain={domain ? domain : ""}
+    clientId={clientId ? clientId : ""}
     redirectUri={window.location.origin}
   >
     <Provider store={store}>
@@ -29,6 +34,7 @@ ReactDOM.render(
       </ConnectedRouter>
     </Provider>
   </Auth0Provider>,
-  document.getElementById('root'));
+  document.getElementById("root")
+);
 
 registerServiceWorker();
