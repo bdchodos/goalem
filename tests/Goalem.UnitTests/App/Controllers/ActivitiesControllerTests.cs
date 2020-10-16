9b7670e3f57ca7309755e422628f5e93d0e8b432
Benjamin Chodos <bdchodos@gmail.com>
2020-10-15 23:42:56 -0400

    Added UnitTests project

---
+++ tests/Goalem.UnitTests/App/Controllers/ActivitiesControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Goalem.App.Controllers;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Goalem.UnitTests.App.Controllers
+{
+	[TestClass]
+	public class ActivitiesControllerTests
+	{
+		[TestMethod("GetErgWorkouts_should_return_10_records")]
+		public void GetErgWorkoutsTest()
+		{
+			var controller = new ActivitiesController(
+				new NullLogger<ActivitiesController>());
+
+			var workouts = controller.GetErgWorkouts(
+				page: 0,
+				pageSize: 10);
+
+			Assert.AreEqual(10, workouts.Count());
+		}
+	}
+}
