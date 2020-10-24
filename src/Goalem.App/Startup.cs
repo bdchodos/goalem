228ecd207d55c47f30e21bd937c206fa53917448
Benjamin Chodos <bdchodos@gmail.com>
2020-10-23 22:59:22 -0400

    startup updates

--- src/Goalem.App/Startup.cs
+++ src/Goalem.App/Startup.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,17 +15,33 @@ namespace Goalem.App
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
+		public Startup(
+			IConfiguration configuration,
+			IWebHostEnvironment environment)
 		{
 			Configuration = configuration;
+			Environment = environment;
 		}
 
 		public IConfiguration Configuration { get; }
 
+		public IWebHostEnvironment Environment { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllersWithViews();
+			IMvcCoreBuilder builder = services
+				.AddMvcCore()
+				.AddApiExplorer()
+				.AddAuthorization()
+				.AddCors()
+				.AddDataAnnotations()
+				.AddFormatterMappings()
+				.AddViews()
+				.AddRazorViewEngine()
+				.AddCacheTagHelper();
+
+			AddTagHelpersFrameworkParts(builder.PartManager);
 
 			// In production, the React files will be served from this directory
 			services.AddSpaStaticFiles(configuration =>
@@ -29,9 +51,9 @@ namespace Goalem.App
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app)
 		{
-			if (env.IsDevelopment())
+			if (Environment.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
@@ -59,11 +81,37 @@ namespace Goalem.App
 			{
 				spa.Options.SourcePath = "ClientApp";
 
-				if (env.IsDevelopment())
+				if (Environment.IsDevelopment())
 				{
 					spa.UseReactDevelopmentServer(npmScript: "start");
 				}
 			});
+		}
+
+		internal static void AddTagHelpersFrameworkParts(ApplicationPartManager partManager)
+		{
+			Assembly mvcTagHelpersAssembly = typeof(InputTagHelper).GetTypeInfo().Assembly;
+			if (!partManager.ApplicationParts.OfType<AssemblyPart>().Any(p => p.Assembly == mvcTagHelpersAssembly))
+			{
+				partManager.ApplicationParts.Add(new FrameworkAssemblyPart(mvcTagHelpersAssembly));
+			}
+
+			Assembly mvcRazorAssembly = typeof(UrlResolutionTagHelper).GetTypeInfo().Assembly;
+			if (!partManager.ApplicationParts.OfType<AssemblyPart>().Any(p => p.Assembly == mvcRazorAssembly))
+			{
+				partManager.ApplicationParts.Add(new FrameworkAssemblyPart(mvcRazorAssembly));
+			}
+		}
+
+		private class FrameworkAssemblyPart : AssemblyPart, ICompilationReferencesProvider
+		{
+			public FrameworkAssemblyPart(Assembly assembly)
+				: base(assembly)
+			{
+			}
+
+			IEnumerable<string> ICompilationReferencesProvider.GetReferencePaths()
+				=> Enumerable.Empty<string>();
 		}
 	}
 }
